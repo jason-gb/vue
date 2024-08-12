37b8353ed5439f5deef66c39bc456f2364e123b6
jason-gb <a01071865081@gmail.com>
2024-08-12 16:29:01 +0900

    수업 시작

---
+++ 01_first/src/App.vue
@@ -0,0 +1,23 @@
+<!-- 보이는 부분 -->
+<template>
+  <main>
+    <h2>App 컴포넌트 생성</h2>
+    <p>{{ message }}</p>
+  </main>
+</template>
+
+<!-- 컴포넌트에서 쓰는 기능 -->
+<script>
+  export default {
+    data: function() {
+      return {
+        message : '안녕하세요오'
+      }
+    }
+  }
+</script>
+
+<!-- 컴포넌트에서 쓰는 디자인 -->
+<style scoped>
+
+</style>
