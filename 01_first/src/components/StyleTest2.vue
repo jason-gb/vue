bff8a5ed057921eee60087671fd74b9f950045d6
jason-gb <a01071865081@gmail.com>
2024-08-19 09:35:52 +0900

    수업 미룬거 정리

--- 01_first/src/components/StyleTest2.vue
+++ 01_first/src/components/StyleTest2.vue
@@ -1,15 +1,30 @@
 <template>
     <div>
-        <h2>CSS 모듈 테스트</h2>
+        <h2 :class="$style.title">CSS 모듈 테스트</h2>
+
+        <div :class="$style.div">
+
+        </div>
     </div>
 </template>
 
-<script>
+<scrip>
     export default {
-        name: 'StyleTest2'
+        name: 'StyleTest2',
+        created () {
+            console.log(this.$style);
+        },
     }
-</script>
+</scrip>
 
-<style scoped>
-
+<style module>
+    .title {
+        background-color: beige;
+        color: brown;
+    }
+    .div {
+        background-color: purple;
+        width: 100px;
+        height: 100px;
+    }
 </style>
