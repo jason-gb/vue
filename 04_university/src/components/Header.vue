dde113e744c14e55e63dc05b717b576b3a731cfd
jason-gb <a01071865081@gmail.com>
2024-08-19 17:42:42 +0900

    수업 정리

---
+++ 04_university/src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+    <header>
+    <ul class="nav">
+        <li class="nav-item">
+        <RouterLink class="nav-link active" aria-current="page" to="/">Home</RouterLink>
+        </li>
+        <li class="nav-item">
+        <RouterLink class="nav-link" to="/departments">학과 목록 조회</RouterLink>
+        </li>
+    </ul>
+    </header>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
---
+++ 04_university/src/pages/DepartmentDetail.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h2>학과 상세 조회</h2>
+        <p>{{ currentRoute.fullPath }}</p>
+        <p>{{ currentRoute.path }}</p>
+        <p>{{ currentRoute.params }}</p>
+        <p>{{ currentRoute.params.no }}</p>
+        <p>{{ currentRoute.query }}</p>
+
+    </div>
+</template>
+
+<script setup>
+    import { useRoute } from 'vue-router';
+
+    const currentRoute = useRoute();
+</script>
+
+<style lang="scss" scoped>
+
+</style>
